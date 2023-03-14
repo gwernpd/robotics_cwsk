ceb0d7113ead4df9e1a321be7c9da61bc43fe443
GeorgeMElling <94914503+GeorgeMElling@users.noreply.github.com>
2023-03-14 11:08:01 +0000

    Update cube_tracker_node.py

--- cube_tracker_node.py
+++ cube_tracker_node.py
@@ -86,9 +86,6 @@ class cubeTracker:
     self.jointPose=data.position
 
 
-
-
-
   # Get data on if the robot is currently moving
   def getStates(self,data):
     if (data.open_manipulator_moving_state=='"STOPPED"'):
@@ -102,17 +99,20 @@ class cubeTracker:
   def aimCamera(self):
     if self.readyToMove==True: # If the robot state is not moving
 
-      # Extremely simple - aim towards the target using joints [0] and [3]
+      # Extremely simple - aim towards the target using joints [0], [2] and [3]
       if (abs(self.targetY-0.5)>0.1):
         self.jointRequest.position[3]=self.jointPose[3]+(self.targetY-0.5)
 
       if (abs(self.targetX-0.5)>0.1):
         self.jointRequest.position[0]=self.jointPose[0]-(self.targetX-0.5)
 
+      # Adjust the z coordinate of the end effector
+      if (abs(self.targetX-0.5)>0.1):
+        self.jointRequest.position[2] =self.jointPose[0]-(self.targetX-0.5)  # Set a fixed z value for now
+
       # This command sends the message to the robot
       self.setPose(str(),self.jointRequest,1.0)
       rospy.sleep(1) # Sleep after sending the service request as you can crash the robot firmware if you poll too fast
-
 
 
   # Find the normalised XY co-ordinate of a cube
@@ -122,6 +122,7 @@ class cubeTracker:
     area=[]
     coX=[]
     coY=[]
+    coZ=[]
 
     # Get the red cubes
     for c in range(len(data.cubes)):
@@ -129,25 +130,31 @@ class cubeTracker:
         area.append(data.cubes[c].area)
         coX.append(data.cubes[c].normalisedCoordinateX)
         coY.append(data.cubes[c].normalisedCoordinateY)
+        coZ.append(data.cubes[c].normalisedCoordinateZ)
 
       if (data.cubes[c].cube_colour=='blue'):
         area.append(data.cubes[c].area)
         coX.append(data.cubes[c].normalisedCoordinateX)
         coY.append(data.cubes[c].normalisedCoordinateY)
+        coZ.append(data.cubes[c].normalisedCoordinateZ)
       
       if (data.cubes[c].cube_colour=='yellow'):
         area.append(data.cubes[c].area)
         coX.append(data.cubes[c].normalisedCoordinateX)
         coY.append(data.cubes[c].normalisedCoordinateY)
-    
-    # Find the biggest red cube
-    if (len(area))>0:
-      index_max = max(range(len(area)), key=area.__getitem__)
-      self.targetX=coX[index_max]
-      self.targetY=coY[index_max]
-    else: # If you dont find a target, report the centre of the image to keep the camera still
+        coZ.append(data.cubes[c].normalisedCoordinateZ)
+
+    # If no red cubes detected, set target to center
+    if len(coX)==0:
       self.targetX=0.5
       self.targetY=0.5
+      self.targetZ=0.2
+    else:
+      # Track the biggest red cube
+      biggest=np.argmax(area)
+      self.targetX=coX[biggest]
+      self.targetY=coY[biggest]
+      self.targetZ=coZ[biggest]
 
 # Main 
 def main(args):
