4421cb9e74fba236fe5f35f4ea07344bb46ea353
gwernpd <94914431+gwernpd@users.noreply.github.com>
2023-05-09 15:08:16 +0100

    Update cube_tracker_node.py

--- cube_tracker_node.py
+++ cube_tracker_node.py
@@ -15,10 +15,14 @@ from cube_spotter.msg import cubeData
 from cube_spotter.msg import cubeArray
 import numpy as np;
 import math
-
+from ikpy.chain import Chain
+from ikpy.link import OriginLink, URDFLink
 from open_manipulator_msgs.msg import *
 from open_manipulator_msgs.srv import *
 detected = False
+global result
+
+test = True
 
 
 
@@ -30,6 +34,25 @@ FOCAL_LENGTH = 850  # You may need to calibrate your camera to find this value
 
 class cubeTracker:
   def __init__(self):
+    self.my_chain = Chain(name='my_chain', links=[
+            OriginLink(),
+            URDFLink(
+              name="joint_1",
+              bounds=[-3.14159, 3.14159],
+              origin_translation=[0, 0, 1.57],
+              origin_orientation=[0, 0, 0],
+              rotation=[0, 0, 1],
+            ),
+            URDFLink(
+              name="joint_2",
+              bounds=[-3.14159, 3.14159],
+              origin_translation=[0, 0, 1.57],
+              origin_orientation=[0, 0, 0],
+              rotation=[0, 1, 0],
+            ),
+            # Add more joints here
+        ])
+
     
     # Where the block is in the image (start at the centre)
     self.targetX=0.5
@@ -79,13 +102,14 @@ class cubeTracker:
     self.setPose = rospy.ServiceProxy('goal_joint_space_path', SetJointPosition)    
     self.jointRequest=JointPosition()
     self.jointRequest.joint_name=["joint1","joint2","joint3","joint4"]  
-    self.jointRequest.position=[-1.9,-1.352,0.379,1.906]
+    self.jointRequest.position=[0,-1.352,0.379,1.906]
+    #self.jointRequest.position=[-1.9,-1.352,0.379,1.906]
     self.setPose(str(), self.jointRequest,5.0)
     rospy.sleep(5) #
     global joint1
     joint1 = -1.9
 
-
+  
 
   # Get the robot's joint positions
   def getJoints(self,data):
@@ -111,7 +135,7 @@ class cubeTracker:
 
 
   # Using the data from all the subscribers, call the robot's services to move the end effector
-  def pick_up(self):
+  def pick_up(self,result):
     print("Picking Up")
     self.setPose = rospy.ServiceProxy('goal_joint_space_path_to_kinematics_pose', SetKinematicsPose)
     self.poseRequest=KinematicsPose()
@@ -120,7 +144,7 @@ class cubeTracker:
     self.poseRequest.pose.position.z = 0.185
     self.setPose(str(),'gripper',self.poseRequest,3.0)
     rospy.sleep(1)
-
+    '''
 
     self.poseRequest=KinematicsPose()
     self.poseRequest.pose.position.x = 0.276
@@ -169,35 +193,41 @@ class cubeTracker:
     self.poseRequest.pose.position.z = 0.035
     self.setPose(str(),'gripper',self.poseRequest,3.0)
     rospy.sleep(5)
+    '''
+
+    #self.gripperRequest=JointPosition()
+    #self.gripperRequest.joint_name=["gripper"]  
+    #self.gripperRequest.position=[0.01]# -0.01 represents closed
+    #self.setGripper(str(),self.gripperRequest,1.0)
+    #rospy.sleep(5)
 
 
-    self.gripperRequest=JointPosition()
-    self.gripperRequest.joint_name=["gripper"]  
-    self.gripperRequest.position=[0.01]# -0.01 represents closed
-    self.setGripper(str(),self.gripperRequest,1.0)
+    #self.poseRequest=KinematicsPose()
+    #self.poseRequest.pose.position.x = 0.239
+    #self.poseRequest.pose.position.y = -0.111
+    #self.poseRequest.pose.position.z = 0.118
+    #self.setPose(str(),'gripper',self.poseRequest,2.0)
+    #rospy.sleep(2)
+
+
+    #self.poseRequest=KinematicsPose()
+    #self.poseRequest.pose.position.x = 0.118
+    #self.poseRequest.pose.position.y = -0.05
+    #self.poseRequest.pose.position.z = 0.18
+    #self.setPose(str(),'gripper',self.poseRequest,2.0)
+    #rospy.sleep(2)
+    self.poseRequest=KinematicsPose()
+    self.poseRequest.pose.position.x = result[0]/2
+    self.poseRequest.pose.position.y = result[1]/2
+    self.poseRequest.pose.position.z = result[2]/2
+    self.setPose(str(),'gripper',self.poseRequest,5)
     rospy.sleep(5)
-
-
+    print(result)
     self.poseRequest=KinematicsPose()
-    self.poseRequest.pose.position.x = 0.239
-    self.poseRequest.pose.position.y = -0.111
-    self.poseRequest.pose.position.z = 0.118
-    self.setPose(str(),'gripper',self.poseRequest,2.0)
-    rospy.sleep(2)
-
-
-    self.poseRequest=KinematicsPose()
-    self.poseRequest.pose.position.x = 0.118
-    self.poseRequest.pose.position.y = -0.05
-    self.poseRequest.pose.position.z = 0.18
-    self.setPose(str(),'gripper',self.poseRequest,2.0)
-    rospy.sleep(2)
-
-    self.poseRequest=KinematicsPose()
-    self.poseRequest.pose.position.x = 0.134
-    self.poseRequest.pose.position.y = 0
-    self.poseRequest.pose.position.z = 0.239
-    self.setPose(str(),'gripper',self.poseRequest,2.0)
+    self.poseRequest.pose.position.x = result[0]
+    self.poseRequest.pose.position.y = result[1]
+    self.poseRequest.pose.position.z = result[2]
+    self.setPose(str(),'gripper',self.poseRequest,5)
     rospy.sleep(5)
     #if self.readyToMove==True: # If the robot state is not moving
     #  print(self.targetY)
@@ -248,8 +278,76 @@ class cubeTracker:
       # Estimate depth (Z-coordinate)
       perceived_width = math.sqrt(self.targetArea)  # Assuming the block is square
       self.targetZ = (REAL_WIDTH * FOCAL_LENGTH) / perceived_width
-      print(self.targetZ)
+      
+      
+  def calc_postion(self):
+    print("calc_pos")
+    def get_position_matrix(x, y, z):
+        print("get_pos")
+        # Let's say you have the coordinates x, y, z of the block in the camera frame
+        # You need to transform these coordinates to the robot frame using a transformation matrix
+        # The transformation matrix depends on the relative position and orientation of the camera with respect to the robot
+        # For example, let's assume the camera is positioned at [1, 0, 0] with respect to the robot and there is no rotation
+        transformation_matrix = np.array([
+            [1, 0, 0, 1],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ])
 
+        # Convert the block coordinates to homogeneous coordinates
+        block_coordinates_camera_frame = np.array([x, y, z, 1])
+
+        # Transform the block coordinates to the robot frame
+        block_coordinates_robot_frame = np.dot(transformation_matrix, block_coordinates_camera_frame)
+
+        # Use IKPy to find the position matrix
+        return block_coordinates_robot_frame
+    block_coordinates_robot_frame = get_position_matrix(self.targetX, self.targetY, self.targetZ)
+    def base_grip():
+        print("base_grip")
+        # Define the transformation matrices
+        θ1 = -1.899
+        θ2 = -1.356
+        θ3 = 0.388
+        θ4 = 1.908
+        d1 = 0.077
+        a1 = 0
+        a2 = 0.13
+        a3 = 0.135
+        a4 = 0.126
+
+        T01 = np.array([[np.cos(θ1), 0, np.sin(θ1), 0],
+                        [np.sin(θ1), 0, -np.cos(θ1), 0],
+                        [0, 1, 0, d1],
+                        [0, 0, 0, 1]])
+
+        T12 = np.array([[np.cos(θ2), -np.sin(θ2), 0, a2*np.cos(θ2)],
+                        [np.sin(θ2), np.cos(θ2), 0, a2*np.sin(θ2)],
+                        [0, 0, 1, 0],
+                        [0, 0, 0, 1]])
+
+        T23 = np.array([[np.cos(θ3), -np.sin(θ3), 0, a3*np.cos(θ3)],
+                        [np.sin(θ3), np.cos(θ3), 0, a3*np.sin(θ3)],
+                        [0, 0, 1, 0],
+                        [0, 0, 0, 1]])
+
+        T34 = np.array([[np.cos(θ4), -np.sin(θ4), 0, a4*np.cos(θ4)],
+                        [np.sin(θ4), np.cos(θ4), 0, a4*np.sin(θ4)],
+                        [0, 0, 1, 0],
+                        [0, 0, 0, 1]])
+
+        # Multiply the matrices
+        T04 = T01 @ T12 @ T23 @ T34
+        return T04
+    T04 = base_grip()
+    global result
+    result = T04@block_coordinates_robot_frame 
+    result = result[:3]
+    result[0] = result[0] + 0.6
+    result[1] = result[1] + 0.484
+    result[2] = result[2] - 1.587
+    return result
 
 # Main 
 def main(args):
@@ -258,12 +356,13 @@ def main(args):
   joint1 = -1.9
   try:
     while not rospy.is_shutdown():
+      result = ic.calc_postion()
       if detected == False:
         #ic.search(joint1=joint1)
         joint1 += 0.1
-      if detected == True:
-        print('Detected')
-        #ic.pick_up()
+      #if detected == True:
+      #  print('Detected')
+      ic.pick_up(result)
       #ic.move_towards()
       #if pick_up == True:
       #  ic.picking_up()
